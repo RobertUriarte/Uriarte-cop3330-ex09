14a854711a0fbf601d014978182d8eb4383ecf24
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-04 15:32:59 -0400

    Maven

--- src/main/java/Exercise9.java
+++ src/main/java/Exercise9.java
@@ -8,15 +8,15 @@ public class Exercise9 {
     static int gallon_size = 350;
     public static void main(String[] args) { //Program to determine number of pizzas per person at pizza party
         Scanner scan = new Scanner(System.in);
-        System.out.printf("Enter the length of ceiling: ");
+        System.out.print("Enter the length of ceiling: ");
         int length = scan.nextInt();
-        System.out.printf("Enter the width of the ceiling: ");
+        System.out.print("Enter the width of the ceiling: ");
         int width = scan.nextInt();
 
         int sqr_feet_of_ceiling = length * width;
 
         if(sqr_feet_of_ceiling  == 0) {
-            System.out.printf("You don't even have a roof");
+            System.out.print("You don't even have a roof");
         }
         else if(sqr_feet_of_ceiling == gallon_size){
             System.out.printf("You will need to purchase 1 gallon of paint for %d square feet", sqr_feet_of_ceiling);
